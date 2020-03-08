47bc4a3ffc9eb2fe8dbef81862f129d53ab41839
srpillai <srpillai@hotmail.com>
2020-03-08 12:47:15 -0500

    add fragments

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,30 +4,43 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Radhakrishnan Pillai
- * for LaunchCode, Kansas City, MO
- * as part of LC-101 Web Development
+ * Created by Radhakrishnan Pillai for LaunchCode, Kansas City, MO as part of LC-101 Web
+ * Development
  */
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-   @GetMapping
-    public String displayAllEvents(Model model){
-       List<String> events = new ArrayList<>();
-       events.add("Code with Pride");
-       events.add("LaunchCode Discovery");
-       events.add("Strange Loop");
-       events.add("Apple WWDC");
-       events.add("SpringOne Platform");
-       model.addAttribute("events", events);
-       return  "events/index";
-   }
+  private static List<String> events = new ArrayList<>();
+
+  @GetMapping
+  public String displayAllEvents(Model model) {
+    model.addAttribute("events", events);
+    return "events/index";
+  }
+
+  //renders at events/create
+  @GetMapping("create")
+  public String renderCreateEventForm() {
+
+    return "events/create";
+  }
+
+  //renders at events/create
+  @PostMapping("create")
+  public String createEvent(@RequestParam String eventName) {
+    events.add(eventName);
+    return "redirect:";
+    //return "create";
+
+  }
 }
